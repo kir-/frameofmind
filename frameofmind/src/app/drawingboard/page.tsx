9eb77a01389e229d5e1a8d23c6d0027ba56e5d73
graysonl <graysonl@sfu.ca>
2024-05-18 15:58:20 -0700

    basic drawing page

---
+++ frameofmind/src/app/drawingboard/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react';
+import 'react-canvas-paint/dist/index.css';
+import dynamic from 'next/dynamic';
+
+const ReactCanvasPaintNoSSR = dynamic(() => import('react-canvas-paint'), { ssr: false });
+
+function DrawingBoard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-8">Drawing Board</h1>
+      <div className="w-full max-w-xs"> {/* Set the maximum width of the canvas container */}
+        <ReactCanvasPaintNoSSR/>
+      </div>
+    </main>
+   
+  );
+}
+
+export default DrawingBoard;
